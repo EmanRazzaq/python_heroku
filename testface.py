81f709425af9b28fb47c874528b924cfa04889e1
Eman <emanaiman2211@gmail.com>
2021-04-07 20:43:50 +0500

    your message

--- testface.py
+++ testface.py
@@ -2,17 +2,7 @@ import face_recognition
 import os
 
 
-from selenium import webdriver
-op = webdriver.ChromeOption()
-op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-op.add_argument("--headless")
-op.add_argument("--no-sandbox")
-op.add_argument("--disable-dev-sh-usage")
 
-driver = webdriver.Chrome(executable_path = os.environ.get("CHROMMEDRIVER_PATH"), chrome_option=op)
-
-driver.get("https://youtube.com")
-print(driver.page_source)
 
 
 known_image = face_recognition.load_image_file(r"C:\Users\htc\Desktop\face-recognition-opencv\face-recognition-opencv\dataset\Sarah_Khan\5.jpeg")
